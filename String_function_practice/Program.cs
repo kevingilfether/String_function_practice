c8cd166eb0af902cffa77b43db4f501458f4e736
kevingilfether <kevingilfether@gmail.com>
2017-10-11 21:35:48 -0400

    Completed IsUnique()

--- String_function_practice/Program.cs
+++ String_function_practice/Program.cs
@@ -10,6 +10,24 @@ namespace String_function_practice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(IsUnique("adva"));
         }
+        public static bool IsUnique(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            char temp;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (j == i)
+                        continue;
+                    else if (inputArray[i] == inputArray[j])
+                        return false;                       
+                }
+            }
+            return true;
+        }
+
     }
 }
